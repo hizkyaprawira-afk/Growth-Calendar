41d6b833617d54a0cb98b6a8a44d69a25f320356
HERSEL29 <barub2772@gmail.com>
2026-03-18 23:18:18 +0700

    Prog1.2. Filling configs.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# Call it at the top of your app
+local_css("assets/style.css")
+
+st.title("Growth Calendar")
